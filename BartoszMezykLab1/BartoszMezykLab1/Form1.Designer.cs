95636a4cc4609d2e774e806dd690764af5ebbf2a
Bartosz Mężyk <bartmezyk@gmail.com>
2019-04-08 07:46:16 +0200

    Dodanie projektu zawierającego pliki realizowane na zajęciach 1. w dniu 04.04.19

---
+++ BartoszMezykLab1/BartoszMezykLab1/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace BartoszMezykLab1
+{
+    partial class FormName
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelName = new System.Windows.Forms.Label();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.buttonChangeColors = new System.Windows.Forms.Button();
+            this.buttonChangeName = new System.Windows.Forms.Button();
+            this.textBoxNumber = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxShowNumber = new System.Windows.Forms.TextBox();
+            this.timerCounter = new System.Windows.Forms.Timer(this.components);
+            this.textBoxShowTimer = new System.Windows.Forms.TextBox();
+            this.buttonRunTimer = new System.Windows.Forms.Button();
+            this.buttonNewForm = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Font = new System.Drawing.Font("Sitka Small", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelName.ForeColor = System.Drawing.Color.Maroon;
+            this.labelName.Location = new System.Drawing.Point(12, 9);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(507, 88);
+            this.labelName.TabIndex = 0;
+            this.labelName.Text = "Bartosz Mężyk";
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.buttonClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonClose.Font = new System.Drawing.Font("Segoe Script", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonClose.Location = new System.Drawing.Point(560, 403);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(228, 67);
+            this.buttonClose.TabIndex = 1;
+            this.buttonClose.Text = "Zamknij";
+            this.buttonClose.UseVisualStyleBackColor = false;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // buttonChangeColors
+            // 
+            this.buttonChangeColors.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.buttonChangeColors.Font = new System.Drawing.Font("Segoe Script", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonChangeColors.Location = new System.Drawing.Point(27, 100);
+            this.buttonChangeColors.Name = "buttonChangeColors";
+            this.buttonChangeColors.Size = new System.Drawing.Size(226, 67);
+            this.buttonChangeColors.TabIndex = 2;
+            this.buttonChangeColors.Text = "Zmień kolory";
+            this.buttonChangeColors.UseVisualStyleBackColor = false;
+            this.buttonChangeColors.Click += new System.EventHandler(this.buttonChange_Click);
+            // 
+            // buttonChangeName
+            // 
+            this.buttonChangeName.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.buttonChangeName.Font = new System.Drawing.Font("Segoe Script", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonChangeName.Location = new System.Drawing.Point(27, 173);
+            this.buttonChangeName.Name = "buttonChangeName";
+            this.buttonChangeName.Size = new System.Drawing.Size(226, 67);
+            this.buttonChangeName.TabIndex = 5;
+            this.buttonChangeName.Text = "Zmień napis";
+            this.buttonChangeName.UseVisualStyleBackColor = false;
+            this.buttonChangeName.Click += new System.EventHandler(this.buttonChangeName_Click);
+            // 
+            // textBoxNumber
+            // 
+            this.textBoxNumber.Location = new System.Drawing.Point(27, 246);
+            this.textBoxNumber.Name = "textBoxNumber";
+            this.textBoxNumber.Size = new System.Drawing.Size(226, 22);
+            this.textBoxNumber.TabIndex = 6;
+            this.textBoxNumber.Text = "1";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Font = new System.Drawing.Font("Segoe Script", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(27, 274);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(226, 67);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Wyświetl";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxShowNumber
+            // 
+            this.textBoxShowNumber.Location = new System.Drawing.Point(27, 347);
+            this.textBoxShowNumber.Name = "textBoxShowNumber";
+            this.textBoxShowNumber.Size = new System.Drawing.Size(226, 22);
+            this.textBoxShowNumber.TabIndex = 9;
+            this.textBoxShowNumber.Text = "0";
+            // 
+            // timerCounter
+            // 
+            this.timerCounter.Interval = 1000;
+            this.timerCounter.Tick += new System.EventHandler(this.timerCounter_Tick);
+            // 
+            // textBoxShowTimer
+            // 
+            this.textBoxShowTimer.Location = new System.Drawing.Point(27, 375);
+            this.textBoxShowTimer.Name = "textBoxShowTimer";
+            this.textBoxShowTimer.Size = new System.Drawing.Size(226, 22);
+            this.textBoxShowTimer.TabIndex = 10;
+            this.textBoxShowTimer.Text = "0";
+            // 
+            // buttonRunTimer
+            // 
+            this.buttonRunTimer.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.buttonRunTimer.Font = new System.Drawing.Font("Segoe Script", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonRunTimer.Location = new System.Drawing.Point(27, 403);
+            this.buttonRunTimer.Name = "buttonRunTimer";
+            this.buttonRunTimer.Size = new System.Drawing.Size(228, 67);
+            this.buttonRunTimer.TabIndex = 11;
+            this.buttonRunTimer.Text = "Włącz timer";
+            this.buttonRunTimer.UseVisualStyleBackColor = false;
+            this.buttonRunTimer.Click += new System.EventHandler(this.buttonRunTimer_Click);
+            // 
+            // buttonNewForm
+            // 
+            this.buttonNewForm.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.buttonNewForm.Font = new System.Drawing.Font("Segoe Script", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonNewForm.Location = new System.Drawing.Point(560, 103);
+            this.buttonNewForm.Name = "buttonNewForm";
+            this.buttonNewForm.Size = new System.Drawing.Size(228, 67);
+            this.buttonNewForm.TabIndex = 12;
+            this.buttonNewForm.Text = "Nowe okno";
+            this.buttonNewForm.UseVisualStyleBackColor = false;
+            this.buttonNewForm.Click += new System.EventHandler(this.buttonNewForm_Click);
+            // 
+            // FormName
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 530);
+            this.Controls.Add(this.buttonNewForm);
+            this.Controls.Add(this.buttonRunTimer);
+            this.Controls.Add(this.textBoxShowTimer);
+            this.Controls.Add(this.textBoxShowNumber);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxNumber);
+            this.Controls.Add(this.buttonChangeName);
+            this.Controls.Add(this.buttonChangeColors);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.labelName);
+            this.Name = "FormName";
+            this.Text = "1 zajęcia";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.Button buttonChangeColors;
+        private System.Windows.Forms.Button buttonChangeName;
+        private System.Windows.Forms.TextBox textBoxNumber;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxShowNumber;
+        private System.Windows.Forms.Timer timerCounter;
+        private System.Windows.Forms.TextBox textBoxShowTimer;
+        private System.Windows.Forms.Button buttonRunTimer;
+        private System.Windows.Forms.Button buttonNewForm;
+    }
+}
+
---
+++ BartoszMezykLab1/BartoszMezykLab1/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BartoszMezykLab1
+{
+    public partial class FormName : Form
+    {
+        // Zmienna do której będziemy dodawać wartość z textBoxNumber
+        int counter;
+
+        // Zmienna zwiększająca swoją wartość w zależności od ilości upłyniętych sekund w timerCounter
+        int timerValue=0;
+
+        // Zmienna do stworzenia nowego okna
+        FormAbout formAbout;
+
+        /// <summary>
+        /// Okno główne
+        /// </summary>
+        public FormName()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Funkcja zamykająca okno
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            // Zamykanie okna
+            Close();
+        }
+
+        /// <summary>
+        /// Funkcja zmieniająca kolor tła przycisku buttonClose oraz okna FormName
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonChange_Click(object sender, EventArgs e)
+        {
+            // Zmiana koloru przycisku
+            buttonClose.BackColor = Color.Red;
+
+            // Zmiana koloru okna
+            BackColor = SystemColors.HotTrack;
+        }
+
+        /// <summary>
+        /// Funkcja zmieniająca napis imię i nazwisko na numer zajęć
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonChangeName_Click(object sender, EventArgs e)
+        {
+            // Zmiana napisu
+            labelName.Text = "Laboratorium 1";
+        }
+
+        /// <summary>
+        /// Funkcja pobierająca wartość z testBoxNumber i wyświetlająca na ekranie
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Przypisanie nowej wartości z textBoxNumber
+            int newNamber = Int32.Parse(textBoxNumber.Text);
+
+            // Zwiększenie wartości zmiennej counter o wartość z textBoxNumber
+            counter += newNamber;
+
+            // Wyświetlenie aktualnej wartości zmiennej counter w textBoxShowNumber
+            textBoxShowNumber.Text = counter.ToString() ;
+
+            // Jeśli wartość zmiennej counter jest większa niż 5, to wyświetlenie odpowiedniego komunikatu i zmiana koloru tła
+            if (counter>5)
+            {
+                // Komunikat
+                MessageBox.Show("Wartość jest większa niż 5");
+
+                // Zmiana koloru tła
+                BackColor = SystemColors.ControlDark;
+            }
+        }
+
+        /// <summary>
+        /// Funkcja obsługująca pracę timerCounter
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void timerCounter_Tick(object sender, EventArgs e)
+        {
+            // Zwiększenie wartości zmiennej
+            timerValue++;
+
+            // Wyświetlenie wartości zmiennej timerValue w textBoxShowTimer
+            textBoxShowTimer.Text = timerValue.ToString();
+
+            // Zmiana rozmiaru przycisku buttonClose
+            buttonClose.Height = buttonClose.Height + 1;
+            buttonClose.Width = buttonClose.Width + 2;
+        }
+
+        /// <summary>
+        /// Funkcja włączająca timerCounter
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonRunTimer_Click(object sender, EventArgs e)
+        {
+            // Włączenie timerCounter
+            timerCounter.Start();
+        }
+
+        /// <summary>
+        /// Funkcja tworząca i wyświetlająca nowe okno
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonNewForm_Click(object sender, EventArgs e)
+        {
+            // Stworzenie i wyświetlenie nowego okna
+            formAbout = new FormAbout();
+            formAbout.Show();
+        }
+    }
+}
